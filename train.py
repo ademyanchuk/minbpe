feae5ba0d02e56a8fd9f6f52778959e5f9b9c6ef
Alexey Demyanchuk <demyanchuk.alexey@mh-hannover.de>
2025-04-10 15:11:04 +0200

    Adjust to run script on both tokenizers

--- train.py
+++ train.py
@@ -2,19 +2,20 @@
 this file is to check tokenizer/s functionality.
 """
 from pathlib import Path
-from minbpe import BasicTokenizer
+from minbpe import BasicTokenizer, RegexTokenizer
 
 text = open("./tests/taylorswift.txt", 'r', encoding='utf-8').read()
 # check training with small vocabulary size
-vocab_size = 512
-tokenizer = BasicTokenizer()
-tokenizer.train(text, vocab_size, verbose=True)
+vocab_size = 1024
 # create models folder
 models_path = Path(__file__).resolve().parent / 'models'
 models_path.mkdir(exist_ok=True)
-# save tokenizer
-tokenizer.save(models_path / 'basic')
-# load and ensure encoding and decoding works
-tokenizer2 = BasicTokenizer()
-tokenizer2.load(models_path / 'basic')
-print(tokenizer2.decode(tokenizer.encode(text)) == text)
+for name, tokenizer in zip(['basic', 'regex'], [BasicTokenizer, RegexTokenizer]):
+  t1 = tokenizer()
+  t1.train(text, vocab_size, verbose=True)
+  # save tokenizer
+  t1.save(models_path / name)
+  # load and ensure encoding and decoding works
+  t2 = tokenizer()
+  t2.load(models_path / name)
+  print(t2.decode(t1.encode(text)) == text)
