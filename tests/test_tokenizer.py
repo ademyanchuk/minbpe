6d9b1628dda842816d1c99b5a0211979c670ec4b
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2025-04-11 16:18:08 +0200

    Add encode/decode/train tests

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import pytest
+
+from minbpe import BasicTokenizer, RegexTokenizer
+
+# Borrowed from original minbpe repo
+
+# strings to test
+strings = [
+  "",
+  "!",
+  "hello world!!!? (안녕하세요!) lol123 😉",
+  "FILE:taylorswift.txt", # FILE: is handled as a special string in unpack()
+]
+
+def unpack(text):
+  # FILE: special case to not print the entire content of the file,
+  # while running tests `pytest -v .`
+  if text.startswith('FILE:'):
+     file_path = Path(__file__).resolve().parent
+     text = (file_path / text[5:]).read_text()
+  return text
+
+
+#----------------------------------------------------------------
+
+# test encode/decode
+@pytest.mark.parametrize("text", strings)
+@pytest.mark.parametrize("tokenizer", [BasicTokenizer, RegexTokenizer])
+def test_encode_decode(text, tokenizer):
+   text = unpack(text)
+   t = tokenizer()
+   ids = t.encode(text) 
+   assert t.decode(ids) == text
+
+# minor test of train as per wiki
+@pytest.mark.parametrize("tokenizer", [BasicTokenizer, RegexTokenizer])
+def test_wikipedia_example(tokenizer):
+    """
+    Quick unit test, following along the Wikipedia example:
+    https://en.wikipedia.org/wiki/Byte_pair_encoding
+
+    According to Wikipedia, running bpe on the input string:
+    "aaabdaaabac"
+
+    for 3 merges will result in string:
+    "XdXac"
+
+    where:
+    X=ZY
+    Y=ab
+    Z=aa
+
+    Keep in mind that for us a=97, b=98, c=99, d=100 (ASCII values)
+    so Z will be 256, Y will be 257, X will be 258.
+
+    So we expect the output list of ids to be [258, 100, 258, 97, 99]
+    """
+    t = tokenizer()
+    text = "aaabdaaabac"
+    t.train(text, 256 + 3)
+    ids = t.encode(text)
+    assert ids == [258, 100, 258, 97, 99]
+    assert t.decode(t.encode(text)) == text
