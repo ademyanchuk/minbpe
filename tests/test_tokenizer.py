39c831b35fd4f07b6d426c9b9afdecb11748e07c
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2025-04-11 18:14:54 +0200

    Add save/load functions test

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -13,6 +13,9 @@ strings = [
   "FILE:taylorswift.txt", # FILE: is handled as a special string in unpack()
 ]
 
+
+test_text = "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception."
+
 def unpack(text):
   # FILE: special case to not print the entire content of the file,
   # while running tests `pytest -v .`
@@ -62,3 +65,18 @@ def test_wikipedia_example(tokenizer):
     ids = t.encode(text)
     assert ids == [258, 100, 258, 97, 99]
     assert t.decode(t.encode(text)) == text
+
+# test save/load
+@pytest.mark.parametrize("tokenizer", [BasicTokenizer, RegexTokenizer])
+def test_save_load(tokenizer, tmp_path):
+  t = tokenizer()
+  t.train(test_text, 256 + 64)
+  ids = t.encode(test_text)
+  t.save(tmp_path / "tok")
+  # init and load
+  t = tokenizer()
+  t.load(tmp_path / "tok")
+  ids_new = t.encode(test_text)
+  # check we can encode and decode the same thing
+  assert ids == ids_new
+  assert t.decode(ids_new) == test_text
