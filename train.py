87f43c6d1c67b54344ed4e0e53f5d9c4b4e6856c
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2025-04-12 11:45:38 +0200

    Update to write training verbose output to file

--- train.py
+++ train.py
@@ -7,12 +7,18 @@ from minbpe import BasicTokenizer, RegexTokenizer
 text = open("./tests/taylorswift.txt", 'r', encoding='utf-8').read()
 # check training with small vocabulary size
 vocab_size = 1024
+base_path = Path(__file__).resolve().parent
 # create models folder
-models_path = Path(__file__).resolve().parent / 'models'
+models_path = base_path / 'models'
 models_path.mkdir(exist_ok=True)
+# create verbose output files folder
+out_path = base_path / 'out' 
+out_path.mkdir(exist_ok=True)
+# train, save, load, check identity for both tokenizers
 for name, tokenizer in zip(['basic', 'regex'], [BasicTokenizer, RegexTokenizer]):
   t1 = tokenizer()
-  t1.train(text, vocab_size, verbose=True)
+  with (out_path / f'{name}.txt').open(mode='w') as file:
+    t1.train(text, vocab_size, verbose=True, file=file) 
   # save tokenizer
   t1.save(models_path / name)
   # load and ensure encoding and decoding works
